f887160979fbff2a2c3d7cd18b6f7a2ad7d3f5cf
mooyeon-choi <dus1208@ajou.ac.kr>
2019-09-09 15:37:36 +0900

    190909 | New_reboot-django

---
+++ articles/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('new/', views.new),
+    path('create/', views.create),
+    path('<int:article_pk>/', views.detail),
+    path('<int:article_pk>/delete/', views.delete),
+    path('<int:article_pk>/edit/', views.edit),
+    path('<int:article_pk>/update/', views.update),
+]
